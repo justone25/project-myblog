91db1298d3e1f9713d95cb30442dfabdaa36ad16
xly <ly763247862@163.com>
2020-02-13 13:50:59 +0800

    博客系统搭建-日志记录方法调用

--- src/main/java/com/xlydbb/myblog/handler/ControllerExceptionHandler.java
+++ src/main/java/com/xlydbb/myblog/handler/ControllerExceptionHandler.java
@@ -5,9 +5,9 @@ import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.ModelAndView;
+        import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
+        import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 @Slf4j
--- src/main/java/com/xlydbb/myblog/web/IndexController.java
+++ src/main/java/com/xlydbb/myblog/web/IndexController.java
@@ -3,16 +3,17 @@ package com.xlydbb.myblog.web;
 import com.xlydbb.myblog.exception.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
-    @GetMapping("/")
-    public String index(){
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id,@PathVariable String name){
 //        String blog = null;
 //        if(blog == null){
 //            throw new NotFoundException("博客不存在");
 //        }
-        int i = 9/0;
+        System.out.println("-----------index-------------");
         return "index";
     }
 }
